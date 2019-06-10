9924d26ba94c1c434ae26d66ebf7db1e16ee7c4d
Lauren Davidson <laurendavidson@Laurens-MacBook-Pro.local>
2019-06-10 10:43:13 -0400

    new loadQuiz function

--- src/shared/atfetch.js
+++ src/shared/atfetch.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 async function loadQuiz() {
 
-  const gameLabBase = new Airtable({apiKey: process.env.GAMELAB_AT_KEY}).base(process.env.GAMELAB_BASE_ID)
+  const ldBase = new Airtable({apiKey: process.env.LD_AT_KEY}).base(process.env.LD_BASE_ID)
 
     let questions = []
     let choices = []
@@ -15,8 +15,8 @@ async function loadQuiz() {
     console.log(`Fetching Quiz Data -----------------------`);
 
 
-    const Qs = gameLabBase('flashcardQs').select({
-          maxRecords: 100,
+    const Qs = ldBase('mtQuestions').select({
+          maxRecords: 500,
           view: "Grid view"
       }).eachPage(function page(records, fetchNextPage) {
 
@@ -28,42 +28,15 @@ async function loadQuiz() {
 
       }).then(() => {return 'got Qs!'})
 
-    const As = gameLabBase('fcAnswers').select({
-          maxRecords: 100,
-          view: "Grid view"
-      }).eachPage(function page(records, fetchNextPage) {
-
-          records.forEach(function(record) {
-              console.log('Retrieved', record.get('name'));
-              answers.push(record._rawJson)
-          })
-          fetchNextPage()
-
-      }).then(() => {return 'got As!'})
-
-    const Cs = gameLabBase('fcChoices').select({
-          maxRecords: 100,
-          view: "Grid view"
-      }).eachPage(function page(records, fetchNextPage) {
-
-          records.forEach(function(record) {
-              console.log('Retrieved', record.get('name'));
-              choices.push(record._rawJson)
-          })
-          fetchNextPage()
-
-      }).then(() => {return 'got Cs!'})
-
 
     console.log(await Qs)
-    console.log(await As)
-    console.log(await Cs)
 
-    const quizData = matchIDs(questions, answers, choices, makeQsReadable)
-    console.log(JSON.stringify(quizData, null, 4));
-    return quizData
+    // const quizData = matchIDs(questions, answers, choices, makeQsReadable)
+    console.log(JSON.stringify(questions, null, 4));
+    return questions
+
+    }
 
 
-}
 
 export default loadQuiz
